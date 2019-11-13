0972fc4c3ad5b912c9e713b2de09848ce87614fc
Ana Robalo <anameciarobalo@gmail.com>
2019-11-13 19:00:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def long_planeteer_calls(planetter_calls)# code an argument here
   planetter_calls.find do |call|
     if call.length > 4
       return true
+    else
+      return false
     end
   end
 end
